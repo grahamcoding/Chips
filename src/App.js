417a1789a3c18afb91e39c24e8a6316ac63684f3
Daniel Graham <dgraham.sprint@gmail.com>
2022-03-31 20:54:05 -0500

    Code cleanup, comments, minor fixes.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import React, { Component } from 'react';
 
-
+//Main component of the app. Imports the relevant CSS files and applies the the app from here.
+//React-router was implemented but did not end up being used in the shipping build but left in for expandibility.
 
 class App extends Component {
   render() {
